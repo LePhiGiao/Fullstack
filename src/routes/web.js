7b06f55039dcd34bc72f0d20ecdc274f7b323edb
unknown <meomeomeo1235@gmail.com>
2023-08-20 07:58:27 +0700

    Create New Users

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 import express from 'express'
-
 const router = express.Router()
+import homeController from '../controllers/homeController'
 
 const initWebRoute = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('Hello world')
-    })
+    router.get('/', homeController.handleHelloWorld)
+    router.get('/user', homeController.handleUserPage)
+    router.post('/user/create-user', homeController.handleCreateNewUser)
+
+
 
     return app.use('/', router);
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
 import initWebRoute from './routes/web'
+import bodyParser from 'body-parser'
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+//config body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//config viewEngine
 configViewEngine(app)
 
+//init route
 initWebRoute(app)
 
 app.listen(PORT, () => {
