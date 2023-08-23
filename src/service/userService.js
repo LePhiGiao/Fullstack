c91de8a6e7cd85307c4d4c8692f1c7ddd35f0a37
unknown <meomeomeo1235@gmail.com>
2023-08-23 20:43:04 +0700

    sequelize Model, associate

--- src/service/userService.js
+++ src/service/userService.js
@@ -41,6 +41,26 @@ const createNewUser = async (email, password, username) => {
 
 //get list user
 const getUserList = async () => {
+
+    //test relationship
+    let newUser = await db.User.findOne({
+        where: { id: 1 },
+        attributes: ["id", "username"],
+        include: { model: db.Group, attributes: ["name", "description"] },
+        raw: true,
+        nest: true
+
+    })
+    let roles = await db.Role.findAll({
+        include: { model: db.Group, where: { id: 1 } },
+        raw: true,
+        nest: true
+    })
+
+    console.log('>>>> check new User', newUser)
+    console.log('>>>> check new Role', roles)
+
+
     let users = []
     users = await db.User.findAll()
 
